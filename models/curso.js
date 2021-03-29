a8239b6ed6a5f242fd1fda688e7652d72db2de17
PolarM <felix1020304@gmail.com>
2021-03-29 19:26:49 -0400

    Reinicio

--- models/curso.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-//Esta colección define cuales son las materias que serán impartidas en un curso
-const cursoSchema = new Schema({
-    curso: String,
-    materia: [String],
-})
-
-module.exports = mongoose.model('curso',cursoSchema);
